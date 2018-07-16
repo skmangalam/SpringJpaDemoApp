845eb14e05a13a53517447da00f8f77d1fd9fef8
skmangalam <skmangalam14@gmail.com>
2018-07-16 10:04:41 +0530

    added the home page

---
+++ src/main/java/com/skmangalam/springapp1/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.skmangalam.springapp1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getHomePage(Model model){
+        return "home";
+    }
+}
